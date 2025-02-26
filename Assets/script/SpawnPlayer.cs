79f4d64ecc52cb2fc7ff3223321e30ef673f3b65
MaelValin <maelmy19@gmail.com>
2025-02-26 14:37:06 +0100

    jeu

--- Assets/script/SpawnPlayer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SpawnPlayer : MonoBehaviour
-{
-    private void Awake()
-    {
-        GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
-    }
-}
